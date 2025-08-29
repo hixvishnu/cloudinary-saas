aed8f1194d084f0ac8b2f4778a1c3ff3bcef892c
hixvishnu <hixvishnu@gmail.com>
2025-08-29 17:51:36 +0530

    create the next app and add daisyui and clerk auth

---
+++ middleware.ts
@@ -0,0 +1,41 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
+import { NextResponse } from "next/server"
+
+const isPublicRoute = createRouteMatcher(["/sign-in", "/sign-up", "/", "/home"])
+
+const isPublicApiRoute = createRouteMatcher(["/api/videos"])
+
+export default clerkMiddleware(async (auth, req) => {
+  // auth().then(({ userId }) => {
+  //   // Use userId here
+  // }).catch(error => {
+  //   console.error("Error fetching auth:", error);
+  // });
+
+  const { userId } = await auth()
+  const currentUrl = new URL(req.url)
+  const isAccessingDashboard = currentUrl.pathname === "/home"
+  const isApiRequest = currentUrl.pathname.startsWith("/api")
+
+  //If user is logged in and accessing a public route but not the dashboard
+  if (userId && isPublicRoute(req) && !isAccessingDashboard) {
+    return NextResponse.redirect(new URL("/home", req.url))
+  }
+  //not logged in
+  if (!userId) {
+    //If user is not logged in and trying to access protected route
+    if (!isPublicRoute(req) && !isPublicApiRoute(req)) {
+      return NextResponse.redirect(new URL("/sign-in", req.url))
+    }
+
+    //User not logged in and request is for a protected API
+    if (isApiRequest && !isPublicApiRoute(req)) {
+      return NextResponse.redirect(new URL("/sign-in", req.url))
+    }
+  }
+  return NextResponse.next()
+})
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+}
